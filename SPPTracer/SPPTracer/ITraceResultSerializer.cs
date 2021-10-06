1145caac393489a6d957d77cccd18b45c6e18c3d
CatholicCardinal <79603714+CatholicCardinal@users.noreply.github.com>
2021-10-06 10:48:55 +0300

    WorksReady
    -working all functions

---
+++ SPPTracer/SPPTracer/ITraceResultSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPP_Tracer
+{
+    public interface ITraceResultSerializer
+    {
+        string Serialize(List<Threade> traceResult);
+    }
+}
---
+++ SPPTracer/SPPTracer/ITracer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace CPP_Tracer
+{
+    public interface ITracer
+    {
+
+        void StartTrace();
+
+        void StopTrace();
+
+        List<List<TraceResult>> GetTraceResult();
+    }
+}
